172483580e1f2527f806e6f3e19c71f58a01085b
sori883 <service@dlme.biz>
2018-03-18 23:43:41 +0900

    バージョン情報追記
    ？？？

--- Wol/formMain.Designer.cs
+++ Wol/formMain.Designer.cs
@@ -28,45 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnWake = new System.Windows.Forms.Button();
-            this.btnDown = new System.Windows.Forms.Button();
+            this.BtnWake = new System.Windows.Forms.Button();
+            this.BtnDown = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.ToolEdit = new System.Windows.Forms.ToolStripLabel();
+            this.ToolOption = new System.Windows.Forms.ToolStripLabel();
+            this.ToolVersion = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // btnWake
+            // BtnWake
             // 
-            this.btnWake.Location = new System.Drawing.Point(13, 39);
-            this.btnWake.Name = "btnWake";
-            this.btnWake.Size = new System.Drawing.Size(155, 110);
-            this.btnWake.TabIndex = 0;
-            this.btnWake.Text = "起動";
-            this.btnWake.UseVisualStyleBackColor = true;
+            this.BtnWake.Location = new System.Drawing.Point(13, 39);
+            this.BtnWake.Name = "BtnWake";
+            this.BtnWake.Size = new System.Drawing.Size(155, 110);
+            this.BtnWake.TabIndex = 0;
+            this.BtnWake.Text = "起動";
+            this.BtnWake.UseVisualStyleBackColor = true;
+            this.BtnWake.Click += new System.EventHandler(this.BtnWake_Click);
             // 
-            // btnDown
+            // BtnDown
             // 
-            this.btnDown.Location = new System.Drawing.Point(174, 39);
-            this.btnDown.Name = "btnDown";
-            this.btnDown.Size = new System.Drawing.Size(148, 110);
-            this.btnDown.TabIndex = 1;
-            this.btnDown.Text = "シャットダウン";
-            this.btnDown.UseVisualStyleBackColor = true;
+            this.BtnDown.Location = new System.Drawing.Point(174, 39);
+            this.BtnDown.Name = "BtnDown";
+            this.BtnDown.Size = new System.Drawing.Size(148, 110);
+            this.BtnDown.TabIndex = 1;
+            this.BtnDown.Text = "シャットダウン";
+            this.BtnDown.UseVisualStyleBackColor = true;
+            this.BtnDown.Click += new System.EventHandler(this.BtnDown_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolEdit,
+            this.toolStripSeparator2,
+            this.ToolOption,
+            this.toolStripSeparator1,
+            this.ToolVersion});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(334, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // ToolEdit
+            // 
+            this.ToolEdit.Name = "ToolEdit";
+            this.ToolEdit.Size = new System.Drawing.Size(31, 22);
+            this.ToolEdit.Text = "編集";
+            this.ToolEdit.Click += new System.EventHandler(this.ToolEdit_Click);
+            // 
+            // ToolOption
+            // 
+            this.ToolOption.Name = "ToolOption";
+            this.ToolOption.Size = new System.Drawing.Size(51, 22);
+            this.ToolOption.Text = "オプション";
+            this.ToolOption.Click += new System.EventHandler(this.ToolOption_Click);
+            // 
+            // ToolVersion
+            // 
+            this.ToolVersion.Name = "ToolVersion";
+            this.ToolVersion.Size = new System.Drawing.Size(51, 22);
+            this.ToolVersion.Text = "バージョン";
+            this.ToolVersion.Click += new System.EventHandler(this.ToolVersion_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(334, 161);
-            this.Controls.Add(this.btnDown);
-            this.Controls.Add(this.btnWake);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.BtnDown);
+            this.Controls.Add(this.BtnWake);
             this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnWake;
-        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button BtnWake;
+        private System.Windows.Forms.Button BtnDown;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel ToolEdit;
+        private System.Windows.Forms.ToolStripLabel ToolOption;
+        private System.Windows.Forms.ToolStripLabel ToolVersion;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
     }
 }
 
--- Wol/formMain.cs
+++ Wol/formMain.cs
@@ -16,5 +16,35 @@ namespace Wol
         {
             InitializeComponent();
         }
+
+        private void ToolEdit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ToolOption_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// バージョン表示
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ToolVersion_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("バージョン 1.0.0 \r\n https://sori883.com", "バージョン情報",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void BtnWake_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnDown_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
