87c3d5b98a8e9a2e535197a4979b7a0847686343
sori883 <service@dlme.biz>
2018-03-19 03:36:01 +0900

    シャットダウンクラス終わり
    ｓｌｄかｓ：ｌｆｊｄｆｌｋん：；ｓｄｌｆｊｍ：AEL+

---
+++ Wol/Down.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wol
+{
+    class Down
+    {
+        //シャットダウン先を決めるIP
+        ArrayList ips = new ArrayList();
+
+        public Down()
+        {
+            //ipを取得する処理
+        }
+
+        public void Shutdown()
+        {
+            try
+            {
+                ProcessStartInfo psi = new ProcessStartInfo();
+                psi.CreateNoWindow = true;
+                psi.FileName = "shutdown.exe";
+
+                for(int i = 0; i < ips.Count; i++)
+                {
+                    psi.Arguments = "-s -t 5 -c \"5秒後にシャットダウンします。\" -m " + ips[i];
+                    Process p = Process.Start(psi);
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex.Message);
+            }
+        }
+    }
+}
