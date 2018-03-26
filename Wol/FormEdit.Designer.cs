02728cb0fa88f91811e46b7844b4f234a05dc98d
sori883 <service@dlme.biz>
2018-03-27 01:48:19 +0900

    書き込み

--- Wol/FormEdit.Designer.cs
+++ Wol/FormEdit.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(581, 652);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
--- Wol/XmlWriter.cs
+++ Wol/XmlWriter.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml;
+﻿using System.Collections;
+using System.Xml;
 
 namespace Wol
 {
@@ -7,7 +8,7 @@ namespace Wol
     /// </summary>
     class XmlWriter
     {
-        XmlDocument doc = new XmlDocument();
+        XmlDocument doc;
 
 
         /// <summary>
@@ -16,15 +17,92 @@ namespace Wol
         /// <param name="xmlUrl>書き込むXMLのパス</param>
         public XmlWriter(string xmlUrl)
         {
-            doc.Load(xmlUrl);
-            SetContent();
-            doc.Save(xmlUrl);
+            doc = new XmlDocument();
         }
 
-        
-        public void SetContent()
+        /// <summary>
+        /// オプション設定保存用
+        /// </summary>
+        /// <param name="url">オプションXMLのパス</param>
+        /// <param name="downTime">保存するシャットダウンまでの時間</param>
+        public void CreateOption(string url, int downTime)
         {
+            doc.Load(url);
             XmlElement root = doc.DocumentElement;
+
+            //XMLの内容を全て削除
+            DeleteContent(url, "option");
+
+            XmlElement elemOption = doc.CreateElement("option");
+            XmlElement elemDownTime = doc.CreateElement("DownTime");
+
+
+            elemDownTime.InnerText = downTime.ToString();
+
+            root.AppendChild(elemOption);
+            elemOption.AppendChild(elemDownTime);
+
+            doc.Save(url);
+        }
+
+
+        /// <summary>
+        /// 編集保存用
+        /// </summary>
+        /// <param name="url">編集XMLのパス</param>
+        /// <param name="names">端末名</param>
+        /// <param name="ips">端末のIP</param>
+        /// <param name="macs">端末のMAC</param>
+        public void CreateEdit(string url, ArrayList names, ArrayList ips, ArrayList macs)
+        {
+            doc.Load(url);
+            XmlElement root = doc.DocumentElement;
+
+            //XMLの内容を全て削除
+            DeleteContent(url ,"pc");
+
+            XmlElement elemPc = doc.CreateElement("pc");
+            XmlElement elemName = doc.CreateElement("name");
+            XmlElement elemIp = doc.CreateElement("ip");
+            XmlElement elemMac = doc.CreateElement("mac");
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                elemName.InnerText = (string)names[i];
+                elemIp.InnerText = (string)ips[i];
+                elemMac.InnerText = (string)macs[i];
+
+                root.AppendChild(elemPc);
+                elemPc.AppendChild(elemName);
+                elemPc.AppendChild(elemIp);
+                elemPc.AppendChild(elemMac);
+            }
+
+            doc.Save(url);
+        }
+
+        /// <summary>
+        /// XMLの内容を一旦削除する
+        /// </summary>
+        /// <param name="url">削除するXMLのパス</param>
+        /// <param name="deleteNode">削除するrootnode</param>
+        public void DeleteContent(string url, string deleteNode)
+        {
+            doc.Load(url);
+            XmlElement root = doc.DocumentElement;
+
+            root = doc.DocumentElement;
+            XmlNodeList node = root.GetElementsByTagName(deleteNode);
+
+            if (node.Count > 0)
+            {
+                for (int i = 0; i < node.Count; i++)
+                {
+                    root.RemoveChild(node.Item(i));
+                }
+            }
+
+            doc.Save(url);
         }
     }
 }
